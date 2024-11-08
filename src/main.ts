17e3beb352f29f838e1c0220dd5a7d23ec28025d
HelenaIsh <elena-ishmatova@mail.ru>
2024-11-08 16:22:06 +0100

    fix: fix validation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 
 const PORT = process.env.PORT;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(PORT);
 }
 bootstrap();
