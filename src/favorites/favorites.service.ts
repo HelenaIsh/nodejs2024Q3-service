ce31fa052287abd77e98e8f950e4961dc9712a40
HelenaIsh <elena-ishmatova@mail.ru>
2024-11-11 14:25:10 +0100

    chore: add descriptive messages

--- src/favorites/favorites.service.ts
+++ src/favorites/favorites.service.ts
@@ -11,40 +11,43 @@ export class FavoritesService {
 
   addTrack(id: string): string {
     this.favorites.tracks.push(id);
-    return id;
+    return `Track with id:${id} was added to favorites`;
   }
 
   getTrackIndex(id: string): number {
     return this.favorites.tracks.findIndex((trackId) => trackId === id);
   }
 
-  deleteTrack(trackIndex: number): void {
+  deleteTrack(trackIndex: number): string {
     if (trackIndex !== -1) this.favorites.tracks.splice(trackIndex, 1);
+    return `Track with  was deleted from favorites`;
   }
 
   addAlbum(id: string): string {
     this.favorites.albums.push(id);
-    return id;
+    return `Album with id:${id} was added to favorites`;
   }
 
   getAlbumIndex(id: string): number {
     return this.favorites.albums.findIndex((albumId) => albumId === id);
   }
 
-  deleteAlbum(albumIndex: number): void {
+  deleteAlbum(albumIndex: number): string {
     if (albumIndex !== -1) this.favorites.albums.splice(albumIndex, 1);
+    return `Album with  was deleted from favorites`;
   }
 
   addArtist(id: string): string {
     this.favorites.artists.push(id);
-    return id;
+    return `Artist with id:${id} was added to favorites`;
   }
 
   getArtistIndex(id: string): number {
     return this.favorites.artists.findIndex((artistId) => artistId === id);
   }
 
-  deleteArtist(artistIndex: number): void {
+  deleteArtist(artistIndex: number): string {
     if (artistIndex !== -1) this.favorites.artists.splice(artistIndex, 1);
+    return `Artist with  was deleted from favorites`;
   }
 }
